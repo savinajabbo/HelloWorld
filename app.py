11f034eb2a4777ca8e16e37ded6b894570621945
savina <savina.jabbo@gmail.com>
2023-12-31 18:25:59 -0800

    emoji face converter
    using a def function and implementing it in the print statement. also using a return value of the converted output.

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-phone = input("Phone: ")
-output = ""
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😊",
+        ":(": "😕"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
 
-numbers = {
-    "0": "Zero",
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four",
-    "5": "Five",
-    "6": "Six",
-    "7": "Seven",
-    "8": "Eight",
-    "9": "Nine"
-}
-
-for digit in phone:
-    output += numbers.get(digit, "!") + " "
-print(output)
+message = input(">")
+print(emoji_converter(message))
